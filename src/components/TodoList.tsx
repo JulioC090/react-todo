55cebb6248b01048323a1dbb88895691c67c80cc
JulioC090 <juliocziviani@gmail.com>
2024-01-29 19:22:41 -0300

    refac: extracted useTodo to a custom hook

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,65 +1,20 @@
-import { useState } from "react";
 import { Todo } from "../Todo";
 import TodoListItem from "./TodoListItem";
 import EnterActionInput from "./EnterActionInput";
+import useTodo from "../hooks/useTodo";
 
 interface TodoListProps {
   intialTodoItens?: Array<Todo>
 }
 
-let lastId = 3;
 
 function TodoList({intialTodoItens}: TodoListProps){
-  const [todosList, setTodosList] = useState<Array<Todo>>(intialTodoItens || []);
-
-  function toggleDone(todoId: number){
-    setTodosList(
-      todosList.map(todo => {
-        if(todo.id !== todoId){
-          return todo;
-        }
-        return {...todo, done: !todo.done};
-      })
-    );
-  }
-
-  function addTodoItem(text: string){
-    setTodosList(
-      [
-        ...todosList, 
-        {
-          id: lastId+1, 
-          description: text, 
-          done: false
-        }
-      ]
-    );
-    lastId++;
-  }
-
-  function deleteTodoItem(todoId: number){
-    setTodosList(
-      todosList.filter((todo)=>
-        todo.id !== todoId
-      )
-    );
-  }
-
-  function editTodoItem(todoId: number, text: string){
-    setTodosList(
-      todosList.map(todo => {
-        if(todo.id !== todoId){
-          return todo;
-        }
-        return {...todo, description: text};
-      })
-    );
-  }
+  const {todos, addTodoItem, deleteTodoItem, editTodoItem, toggleDone} = useTodo(intialTodoItens);
 
   return (
     <div>
       <EnterActionInput onPressEnter={(event) => addTodoItem(event.currentTarget.value)}/>
-      {todosList.map((todo) => {
+      {todos.map((todo) => {
         return (
         <TodoListItem 
           key={todo.id}
---
+++ src/hooks/useTodo.ts
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Todo } from "../Todo";
+
+
+let lastId = 3;
+
+function useTodo(intialTodoItens?: Array<Todo>){
+  const [todos, setTodos] = useState<Array<Todo>>(intialTodoItens || []);
+
+  function toggleDone(todoId: number){
+    setTodos(
+      todos.map(todo => {
+        if(todo.id !== todoId){
+          return todo;
+        }
+        return {...todo, done: !todo.done};
+      })
+    );
+  }
+
+  function addTodoItem(text: string){
+    setTodos(
+      [
+        ...todos, 
+        {
+          id: lastId+1, 
+          description: text, 
+          done: false
+        }
+      ]
+    );
+    lastId++;
+  }
+
+  function deleteTodoItem(todoId: number){
+    setTodos(
+      todos.filter((todo)=>
+        todo.id !== todoId
+      )
+    );
+  }
+
+  function editTodoItem(todoId: number, text: string){
+    setTodos(
+      todos.map(todo => {
+        if(todo.id !== todoId){
+          return todo;
+        }
+        return {...todo, description: text};
+      })
+    );
+  }
+
+  return {todos, toggleDone, addTodoItem, deleteTodoItem, editTodoItem}
+}
+
+export default useTodo;
