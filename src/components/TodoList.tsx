3f116670b540fca5f7b50ba92aae8fe5a0a723eb
JulioC090 <juliocziviani@gmail.com>
2024-01-28 22:29:43 -0300

    refac: extract TodoListItem from existing code

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
-import EditableText from "./EditableText";
-
-type Todo = {
-  id: number,
-  description: string,
-  done: boolean
-}
+import { Todo } from "../Todo";
+import TodoListItem from "./TodoListItem";
 
 interface TodoListProps {
   intialTodoItens?: Array<Todo>
@@ -62,24 +57,19 @@ function TodoList({intialTodoItens}: TodoListProps){
     );
   }
 
-  function handleFinishEdit(todoId: number){
-    return (text: string)=>{
-      editTodoItem(todoId, text);
-    }
-  }
-
   return (
     <div>
       <input type="text" onKeyDown={(event) => addTodoItem(event)}/>
       {todosList.map((todo) => {
         return (
-        <div key={todo.id}>
-          <input type="checkbox" onClick={() => toggleDone(todo.id)} />
-          <EditableText initialText={todo.description} onFinishEdit={handleFinishEdit(todo.id)}/>
-          <span>{todo.done ? " V " : " F "}</span>
-          <button onClick={() => deleteTodoItem(todo.id)}>Deletar</button>
-        </div>); 
-      })}
+        <TodoListItem 
+          key={todo.id}
+          todo={todo}
+          toggleDone={() => toggleDone(todo.id)}
+          editTodoItem={(text) => editTodoItem(todo.id, text)}
+          deleteTodoItem={() => deleteTodoItem(todo.id)}
+        />
+      )})}
     </div>
   );
   
