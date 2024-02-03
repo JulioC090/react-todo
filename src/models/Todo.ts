5345be92d72a2f1b8b25384f3af8e904f61fd7e6
JulioC090 <juliocziviani@gmail.com>
2024-02-03 13:27:50 -0300

    feat: change the id generation of the Todo

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -2,11 +2,9 @@ class Todo {
   public id: string;
   public description: string;
   public done: boolean;
-  private static lastId: number = 0; 
 
   constructor(description: string){
-    Todo.lastId++;
-    this.id = `todo-${Todo.lastId}`;
+    this.id = `todo-${Math.random().toString().slice(2, 7)}`;
     this.description = description;
     this.done = false;
   }
